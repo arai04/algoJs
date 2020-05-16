c63ffc6ee0dcdbb63591f9835b8c074c6cbd3f2c
arai <>
2020-05-16 13:15:33 +0900

    素数列挙の関数追加

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 "use strict";
+
+
 //素数判定                                                                                                                                                                    
 const isPrime = n => {
   if (n < 2) return false;
@@ -8,6 +10,12 @@ const isPrime = n => {
     if (n % i === 0) return false;
   }
   return true;
+}
+//素数列挙
+const limitPrimes = limit => {                                                                                                                                                
+  for (let i =2; i < limit; i++){
+    if(isPrime(i)) console.log(i);
+  }
 }
 
 
