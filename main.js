2170b1219df0c8afe08adbe0c4740b8af70f5890
arai <>
2020-05-16 12:53:26 +0900

    素数判定の関数を追加

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 "use strict";
+//素数判定                                                                                                                                                                    
+const isPrime = n => {
+  if (n < 2) return false;
+  if (n === 2) return true;
+  if (n % 2 === 0) return false;
+  for (let i = 2; i < n; i++){
+    if (n % i === 0) return false;
+  }
+  return true;
+}
+
 
